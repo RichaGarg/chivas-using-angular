428cd3da2a45fedb9ddff34e5d77f89c5c756002
Richa <richag@intelligrape.com>
2015-05-01 13:52:47 +0530

    chnage in section2 js including registration process

--- grails-app/assets/javascripts/user/section2Angular.js
+++ grails-app/assets/javascripts/user/section2Angular.js
@@ -1,8 +1,23 @@
 var submitCodeApp = angular.module('SubmitCodeApp', []);
 
-submitCodeApp.controller('SubmitCodeCtrl', ['$scope', '$modal', '$http', function ($scope, $modal, $http) {
+submitCodeApp.factory('CodeSubmitted', function () {
+    var codeSubmit = {codeSubmitted: false, sharedOnFb: false, success: false, msg: '', nric: '', coupon: '', registerCouponId: 0, requestMessage: 'You’re now in the running to win your own private island. you’ll receive an SMS confirmation shortly.'}
+    return codeSubmit
+});
+
+submitCodeApp.factory('ShowModal', function () {
+    //var showModal = {uniqueCode: false, privacyPolicy:false}
+    var showModal = {uniqueCode: false, privacyPolicy:false, tAndC: false, popupAlert: false, submitCodeMsg: false, shareOnFb: false}
+    return showModal
+});
+
+submitCodeApp.controller('SubmitCodeCtrl', ['$scope', '$modal', '$http', 'CodeSubmitted', 'ShowModal', function ($scope, $modal, $http, codeSubmitted, showModal) {
+    $scope.codeSubmit = codeSubmitted
+    $scope.requestMessage = codeSubmitted.requestMessage
+    $scope.showModal = showModal
+
     $scope.open = function (templateUrl, modalCtrl) {
-        var modalInstance = $modal.open({
+        $modal.open({
             templateUrl: templateUrl,
             controller: modalCtrl,
             backdrop: 'static',
@@ -10,50 +25,181 @@ submitCodeApp.controller('SubmitCodeCtrl', ['$scope', '$modal', '$http', functio
         });
     };
     $scope.openUniqueCodePopUp = function () {
-        $scope.open('templates/submitCodeModal.gsp', SubmitCodePopupCtrl);
+        $scope.showModal.uniqueCode = true
     };
     $scope.showPrivacyPolicyPopUp = function () {
-        $scope.closeUniqueCodePopUp();
-        $scope.open('templates/privacyPolicyModal.gsp', PrivacyPolicyPopupCtrl);
+        $scope.showModal.uniqueCode = false
+        $scope.showModal.privacyPolicy = true
     };
     $scope.showTermsAndConditionsPopUp = function () {
-        $scope.closeUniqueCodePopUp();
-        $scope.open('templates/termsAndConditionModal.gsp', TermsAndConditionPopupCtrl);
+        $scope.showModal.uniqueCode = false
+        $scope.showModal.tAndC = true
     };
+    $scope.showAlertPopUp = function () {
+        $scope.showModal.uniqueCode = false
+        $scope.showModal.popupAlert = true
+    };
+
+    $scope.$watch("codeSubmit.codeSubmitted", function (newValue, oldValue) {
+        if(oldValue != newValue) {
+            if (newValue == true) {
+                console.log(oldValue + ' --value changed code submitted-- ' + newValue)
+                $scope.showModal.uniqueCode = false
+                $scope.showModal.popupAlert = true
+            }
+        }
+    });
+    $scope.$watch("showModal.uniqueCode", function (newValue, oldValue) {
+        if(oldValue != newValue) {
+            if (newValue == true) {
+                console.log(oldValue + ' --value changed unique code-- ' + newValue)
+                $scope.open('templates/submitCodeModal.gsp', SubmitCodePopupCtrl);
+            }
+        }
+    });
+    $scope.$watch("showModal.privacyPolicy", function (newValue, oldValue) {
+        if(oldValue != newValue) {
+            if (newValue == true) {
+                console.log(oldValue + ' --value changed privacy policy-- ' + newValue)
+                $scope.open('templates/privacyPolicyModal.gsp', PrivacyPolicyPopupCtrl);
+            }
+        }
+    });
+    $scope.$watch("showModal.tAndC", function (newValue, oldValue) {
+        if(oldValue != newValue) {
+            if (newValue == true) {
+                console.log(oldValue + ' --value changed t and c-- ' + newValue)
+                $scope.open('templates/termsAndConditionModal.gsp', TermsAndConditionPopupCtrl);
+            }
+        }
+    });
+    $scope.$watch("showModal.popupAlert", function (newValue, oldValue) {
+        if(oldValue != newValue) {
+            if (newValue == true) {
+                console.log(oldValue + ' --value changed popup alert-- ' + newValue)
+                $scope.open('templates/popupAlert.gsp', AlertPopupCtrl);
+            }
+        }
+    });
+    $scope.$watch("showModal.submitCodeMsg", function (newValue, oldValue) {
+        if(oldValue != newValue) {
+            if (newValue == true) {
+                console.log(oldValue + ' --value changed submit code msg-- ' + newValue)
+                $scope.open('templates/submitCodeMsgModal.gsp', ShowSubmitCodeMsgCtrl);
+            }
+        }
+    });
+    /*$scope.shareOnFacebook = function() {
+        console.log('fb url--- ')
+        //shareOnFacebook(serverurl)
+    }*/
+
 }]);
 
-var SubmitCodePopupCtrl = ['$scope', '$modalInstance', '$http', function ($scope, $modalInstance, $http) {
+var SubmitCodePopupCtrl = ['$scope', '$modalInstance', '$http', 'CodeSubmitted', 'ShowModal', function ($scope, $modalInstance, $http, codeSubmitted, showModal) {
+    $scope.codeSubmit = codeSubmitted
+    $scope.showModal = showModal
     $scope.uCode = {}
     $scope.accepted = false
+    $scope.submittedCode = false
     $scope.submitCode = function () {
-        console.log($scope.uCode)
-        $http.post('/user/submitCode', $scope.uCode).
-            success(function (data, status, headers, config) {
-                console.log('1---   ' + data.success)
-                success = data.success
-                if (data.success) {
-                    console.log('success---')
-                    return true;
-                }
-                return false;
-            });
+        var promise = $http.post('/user/submitCode', $scope.uCode);
+        promise.then(function (promise) {
+            $scope.codeSubmit.codeSubmitted = promise.data.success
+            $scope.codeSubmit.msg = promise.data.msg
+            $scope.codeSubmit.requestMessage = promise.data.msg
+            console.log(promise.data.msg + ' ---code submit msg-- ' + $scope.codeSubmit.requestMessage)
+            if (promise.data.success) {
+                $scope.codeSubmit.nric = promise.data.nric
+                $scope.codeSubmit.coupon = promise.data.coupon
+                $scope.codeSubmit.registerCouponId = promise.data.registerCouponId
+                $scope.closeUniqueCodePopUp();
+                //$scope.showModal.popupAlert = true
+            }
+        });
     };
+    $scope.$watch("showModal.uniqueCode", function (newValue, oldValue) {
+        if(oldValue != newValue) {
+            if (newValue == false) {
+                console.log(oldValue + ' --value changed unique code close-- ' + newValue)
+                $modalInstance.close();
+            }
+        }
+    });
     $scope.closeUniqueCodePopUp = function () {
-        $modalInstance.close();
+        $scope.showModal.uniqueCode = false
     };
 }];
 
-var TermsAndConditionPopupCtrl = ['$scope', '$modalInstance', function ($scope, $modalInstance) {
-    $scope.closeTermsAndConditionsPopUp = function () {
-        $modalInstance.close();
-        //$scope.openUniqueCodePopUp();
+var AlertPopupCtrl = ['$scope', '$modalInstance', '$http', 'ShowModal', function ($scope, $modalInstance, $http, showModal) {
+    $scope.showModal = showModal
+    $scope.$watch("showModal.popupAlert", function (newValue, oldValue) {
+        if(oldValue != newValue) {
+            if (newValue == false) {
+                console.log(oldValue + ' --value changed popup alert close-- ' + newValue)
+                $modalInstance.close();
+            }
+        }
+    });
+    $scope.closePopUpAlert = function () {
+        $scope.showModal.popupAlert = false
+        $scope.showModal.submitCodeMsg = true
+    };
+}];
+
+var ShowSubmitCodeMsgCtrl = ['$scope', '$modalInstance', '$http', 'CodeSubmitted', 'ShowModal', function ($scope, $modalInstance, $http, codeSubmitted, showModal) {
+    $scope.codeSubmit = codeSubmitted
+    $scope.showModal = showModal
+    $scope.$watch("showModal.submitCodeMsg", function (newValue, oldValue) {
+        if(oldValue != newValue) {
+            console.log(oldValue + ' --value changed submitcode msg close-- ' + newValue)
+            if (newValue == false) {
+                $modalInstance.close();
+            }
+        }
+    });
+    $scope.closeSubmitCodeMsgPopUp = function () {
+        $scope.showModal.submitCodeMsg = false
+    };
+    $scope.shareOnFacebook = function(serverUrl) {
+        console.log('inside share on fb function')
+        shareOnFacebook(serverUrl)
     }
 }];
 
-var PrivacyPolicyPopupCtrl = ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+
+
+
+var TermsAndConditionPopupCtrl = ['$scope', '$modalInstance', 'ShowModal', function ($scope, $modalInstance, showModal) {
+    $scope.showModal = showModal
+    $scope.$watch("showModal.tAndC", function (newValue, oldValue) {
+        if(oldValue != newValue) {
+            if (newValue == false) {
+                console.log(oldValue + ' --value changed tAndC close-- ' + newValue)
+                $modalInstance.close();
+            }
+        }
+    });
+    $scope.closeTermsAndConditionsPopUp = function () {
+        $scope.showModal.tAndC = false
+        $scope.showModal.uniqueCode = true
+    }
+}];
+
+var PrivacyPolicyPopupCtrl = ['$scope', '$modalInstance', 'ShowModal', function ($scope, $modalInstance, showModal) {
+    $scope.showModal = showModal
+    $scope.$watch("showModal.privacyPolicy", function (newValue, oldValue) {
+        if(oldValue != newValue) {
+            if(newValue == false) {
+                console.log(oldValue + ' --value changed privacy policy closed-- ' + newValue)
+                $modalInstance.close();
+            }
+        }
+
+    });
     $scope.closePrivacyPolicyPopUp = function () {
-        $modalInstance.close();
-        //$scope.openUniqueCodePopUp();
+        $scope.showModal.uniqueCode = true
+        $scope.showModal.privacyPolicy = false
     }
 }];
 
@@ -62,25 +208,6 @@ submitCodeApp.directive('submitUniqueCode', function () {
         restrict: 'E',
         templateUrl: 'templates/section2.gsp'
     }
-});
-
-var PHONE_REGEXP = /^[689]\d{7}$/;
-submitCodeApp.directive('validPhoneNumber', function () {
-    return {
-        require: 'ngModel',
-        link: function (scope, elm, attrs, ctrl) {
-            ctrl.$validators.validPhoneNumber = function (modelValue, viewValue) {
-/*                if (ctrl.$isEmpty(modelValue)) {
-                    return true;
-                }
-                if (PHONE_REGEXP.test(viewValue)) {
-                    return true;
-                }
-                return false;*/
-                return true;
-            };
-        }
-    };
 });
 
 submitCodeApp.directive('validUniqueCode', ['$http', function ($http) {
